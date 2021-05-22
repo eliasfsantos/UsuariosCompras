6485f39c2b3c52aec9bc8b5a9dcbad2d94238a2d
eliasfsantos <eliasfsantos@live.com>
2021-05-22 20:39:35 -0300

    validation

--- src/usuarios/usuarios.controller.ts
+++ src/usuarios/usuarios.controller.ts
@@ -1,35 +1,36 @@
 import { Body, Controller, Get, HttpStatus, Param, Post } from '@nestjs/common';
-import { UsuariosDTO } from './usuarios.dto';
-import { Usuarios } from './usuarios.entity';
-import { UsuariosService } from './usuarios.services';
+import { validate } from 'class-validator';
+
+import { CriarUsuarioDto } from './dto/criarUsuarioDto';
+import { UsuarioService } from './usuario.services';
+import { Usuario } from "src/models/usuario";
+import { Endereco } from 'src/models';
+
 
 @Controller('usuarios')
 export class UsuariosController {
-  constructor(private readonly usuariosService:UsuariosService) {}
+  constructor(private readonly usuarioService: UsuarioService) { }
 
-  @Get()
-  async getUsuarios(): Promise<Usuarios[]>{
-    return this.usuariosService.findAll();
+  @Get('/')
+  buscarUsuarios() {
+    return this.usuarioService.findAll();
   }
 
-  @Get(':id')
-  async readUser(@Param('id') id: number) {
-    return {
-      statusCode: HttpStatus.OK,
-      data: await this.usuariosService.read(id),
-    };
+  @Get('/:id')
+  readUser(@Param('id') id) {
+    return this.usuarioService.findOne(id)
   }
 
-  // @Post()
-  // async criarUsuarios(@Body() data: UsuariosDTO) {
-  //   if(data.senha.length > 11) return {
-  //     statusCode: HttpStatus.CREATED,
-  //     message: 'Usuario adicionado com sucesso',
-  //     data: await this.usuariosService.criar(data),
-  //   };
-  //   else return {
-  //     statusCode: HttpStatus.LENGTH_REQUIRED,
-  //     message: 'senha precisa ter pelo menos 12 caracteres'
-  //   }
-  // }
+  @Post('/')
+  async criarUsuarios(@Body() criarUsuarioDto: CriarUsuarioDto) {
+    if (criarUsuarioDto.confirmacaoDeSenha == criarUsuarioDto.senha) return {
+      statusCode: HttpStatus.CREATED,
+      message: 'Usuario adicionado com sucesso',
+      data: await this.usuarioService.criar(criarUsuarioDto)
+    }
+    else return {
+      statusCode: HttpStatus.NOT_ACCEPTABLE,
+      message: 'senha não confere'
+    }
+  }
 }
--- src/usuarios/usuarios.dto.ts
+++
@@ -1,9 +0,0 @@
-import { Endereco } from './endereco.entity';
-export interface UsuariosDTO {
-    id: number;
-    nome: string;
-    email: string;
-    senha: string;
-    confirmacao_senha: string;
-    endereco: Endereco
-  }
